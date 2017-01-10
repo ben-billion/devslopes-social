c61dfe587f7b740b0a3016e7c6542cbb86aee85e
Benjamin Neal <bneal@Bens-Air.charter.com>
2017-01-09 17:42:14 -0800

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Benjamin Neal on 1/9/17.
+//  Copyright © 2017 Benjamin Neal. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ devslopes-social/FancyView.swift
@@ -0,0 +1,23 @@
+//
+//  FancyView.swift
+//  devslopes-social
+//
+//  Created by Benjamin Neal on 1/9/17.
+//  Copyright © 2017 Benjamin Neal. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: SHADOW_GRAY).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+
+}
