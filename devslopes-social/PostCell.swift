ad5691b57dc73df2f569185361e3b6a4f2b6be1e
Benjamin Neal <bneal@Bens-Air.charter.com>
2017-01-10 15:26:44 -0800

    Completed feed tableview

---
+++ devslopes-social/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  devslopes-social
+//
+//  Created by Benjamin Neal on 1/10/17.
+//  Copyright © 2017 Benjamin Neal. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
